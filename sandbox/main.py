9b96bf48d138ec5328e2fbf45ff2220cbc3b6a20
rifqidewangga <rifdewangga@gmail.com>
2022-03-10 18:23:51 +0700

    added more samples

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+from google.protobuf.json_format import MessageToDict
 
 
 def face_detection_demo_static():
@@ -68,5 +69,84 @@ def face_detection_demo_webcam():
     cap.release()
 
 
+def hands_detection_demo_webcam():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_hands = mp.solutions.hands
+
+    # For webcam input:
+    cap = cv2.VideoCapture(0)
+
+    with mp_hands.Hands(
+            model_complexity=0,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            results = hands.process(image)
+
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+
+
+def hands_detection_analyze_landmark():
+    drawing_module = mp.solutions.drawing_utils
+    hands_module = mp.solutions.hands
+
+    with hands_module.Hands(static_image_mode=True) as hands:
+        image = cv2.imread('samples/img/kira-auf-der-heide-QyCH5jwrD_A-unsplash.jpg')
+
+        # input to hands_module should be mirrored and the color should be in RGB
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.flip(image, 1)
+
+        results = hands.process(image)
+        image_height, image_width, _ = image.shape
+
+        for idx, hand_handedness in enumerate(results.multi_handedness):
+            handedness_dict = MessageToDict(hand_handedness)['classification'][0]
+            print(handedness_dict)
+
+        if results.multi_hand_landmarks is not None:
+            for handLandmarks in results.multi_hand_landmarks:
+                for point in hands_module.HandLandmark:
+                    normalized_landmark = handLandmarks.landmark[point]
+                    pixel_coordinates_landmark = \
+                        drawing_module._normalized_to_pixel_coordinates(
+                            normalized_landmark.x, normalized_landmark.y,
+                            image_width, image_height)
+
+                    print(point)
+                    print(pixel_coordinates_landmark)
+                    print(normalized_landmark)
+
+
 if __name__ == '__main__':
-    face_detection_demo_static()
+    hands_detection_analyze_landmark()
